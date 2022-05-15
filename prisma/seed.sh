8b82cca6681c94397b350e71538fa0d703ad1088
Gabe <gabrielgrechuk@outlook.com>
2022-05-15 10:47:29 -0300

    Removendo variáveis de ambiente e consetando output estilizado do seed.sh

--- prisma/seed.sh
+++ prisma/seed.sh
@@ -8,21 +8,21 @@ BLUE='\033[0;34m'
 NC='\033[0m'
 
 # Baixando arquivos com registro de usuários para subir no servidor.
-echo "${GREEN}Baixando registro de usuários...${NC}"
+printf "${GREEN}Baixando registro de usuários...${NC}\n"
 # Subi no transfer.sh, já que o no google drive não da pra baixar com o curl.
 curl https://transfer.sh/xyLgmo/users.csv.tgz -o users.csv.tgz
-echo "${GREEN}Extraindo arquivo...${NC}"
+printf "${GREEN}Extraindo arquivo...${NC}\n"
 tar -xf users.csv.tgz
-echo "${GREEN}Removendo cabeçalho do CSV...${NC}"
+printf "${GREEN}Removendo cabeçalho do CSV...${NC}\n"
 sed -i '1d' users.csv 
 
-echo "${GREEN}Importando dados com psql...${NC}"
-echo "${BLUE}  Isso provavelmente vai demorar vários minutos"
-echo "  Por favor, aguarde a conclusão do processo.${NC}"
-echo "${RED}!!! ENTRAR SENHA PARA O POSTGRES:${NC} ${BLUE}123123${NC}"
+printf "${GREEN}Importando dados com psql...${NC}\n"
+printf "${BLUE}  >Isso provavelmente vai demorar vários minutos\n"
+printf "  >Por favor, aguarde a conclusão do processo.${NC}\n"
+printf "${RED}!!! ENTRAR SENHA PARA O POSTGRES:${NC} ${BLUE}123123${NC}\n"
 psql -h localhost -d cadastro -U postgres -c "\copy usuarios(id, name, email, phone) FROM './users.csv' DELIMITER ',' CSV HEADER;"
 
-echo "${GREEN}Limpando arquivos desnecessários...${NC}"
+printf "${GREEN}Limpando arquivos desnecessários...${NC}\n"
 rm -f users.csv.tgz users.csv
-echo "${GREEN}Pronto!${NC}"
+printf "${GREEN}Pronto!${NC}\n"
 
