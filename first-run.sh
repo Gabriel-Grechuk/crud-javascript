fdb5afbb50cf2507d12b5b3cb2ab2a4bc6a2e5fd
Gabe <gabrielgrechuk@outlook.com>
2022-05-15 10:46:17 -0300

    Baixando pacotes npm localmente

--- first-run.sh
+++ first-run.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
+npm install
 docker-compose up -d
+printf "Aguardando 5s até o host do postgres estar configurado...\n"
+sleep 5
 npx prisma migrate dev --name init
+
+printf "Servidor rodando! verificar localhost:8080\n"
 
